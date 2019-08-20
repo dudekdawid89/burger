1755f35727e5092d0cb95a3aea811ac464ad1464
dudekdawid89 <dudekdawid89@gmail.com>
2019-08-19 22:22:53 -0500

    insert query set up

--- config/orm.js
+++ config/orm.js
@@ -9,5 +9,14 @@ var orm = {
             cb(result);
         })
     },
-    // insertOne: function()
+    insertOne: function(table, cols, vals, cb){
+        var query = "INSERT INTO "+table+" (burgerName, devoured) values('"+cols+"',"+vals+");";
+        connection.query(query,function(err,result){
+            if(err){
+                throw err;
+            }
+            cb(result);
+        })
+    },
+    
 };
